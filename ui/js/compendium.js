138e361e91499207791445ad6d6066bded704244
Alphaeco <michael@alphaecoconstruction.com>
2017-03-28 22:10:17 -0400

    updated ui files

--- ui/js/compendium.js
+++ ui/js/compendium.js
@@ -2,75 +2,162 @@
  * for compendium browser
  */
 
-shortcut.add("shift+a", function (){
-    alert("shortcut");
-});
-
 $currentTab = "dashboard";
 
 // tabs 
-$(".tab").click(function() {
-    $( this ).siblings().removeClass("active");
-    $( this ).addClass("active");
+var tabToggle = function (tab) {
+	
+    $( tab ).siblings().removeClass("active");
+    $( tab ).addClass("active");
 
-    $currentTab = $(this).attr("id");
-    $targetName = $(this).attr("id") + "_target";
+    $currentTab = $(tab).attr("id");
+    $targetName = $(tab).attr("id") + "_target";
 
     $(".content").children().addClass("hide");
     $target = $("#" + $targetName);
 
     $target.removeClass("hide");
+}
 
+$(".tab").click(function() {
+    tabToggle(this);    
+});
+
+var runActiveLoad = function () {
+                if ($("#header").hasClass("small")){
+                    $("#header").removeClass("small");
+                } else {
+                    $("#header").addClass("small");
+                }
+        };
+               
+        var progressUpdate = function  (value) {
+                $(".progressbar").width("0%").fadeIn("slow");
+                $(".progressbar").animate({
+                    "width": value + "%"
+                }, 5)};
+
+        shortcut.add("shift+h", function(){
+                runActiveLoad();
+        });
+        
+        shortcut.add("ctrl+r", function(){
+                var cnt = 1;
+                while(cnt < 101) {
+                    progressUpdate(cnt);
+                    cnt++
+                }
+        });
+
+shortcut.add("shift+a", function (){
+    if ($currentTab == "media") {
+        $("#toolbar").slideToggle();
+    }
 });
 
 /* media */
 
 var localMedia = [
-    { title: "Star Trek - First Contract",
-       path: "/media/local/video/starTrek/firstContact.mp4",
+    { title: "Oceans",
+       path: "http://vjs.zencdn.net/v/oceans.mp4",
        desc: "desc...",
          id: 1,
        type: "video", 
       cover: "img/video_placeholder.jpg"
     },
-    { title: "What is Life - untitled",
-       path: "/media/local/youtube/life.mp4",
-       desc: "youtube desc...",
+    { title: "Big Bucks Bunny",
+       path: "http://mirrorblender.top-ix.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_surround.avi",
+       desc: "desc...",
+         id: 1,
+       type: "video", 
+      cover: "http://camendesign.com/code/video_for_everybody/poster.jpg"
+    },
+    { title: "Sintel",
+       path: "http://peach.themazzone.com/durian/movies/sintel-2048-stereo.mp4",
+       desc: "desc...",
+         id: 1,
+       type: "video", 
+      cover: "https://durian.blender.org/wp-content/uploads/2010/10/sintel-cover-small1.jpg"
+    },
+    { title: "Local Link",
+       path: "file:///home/tarantula/Videos/video.mkv",
+       desc: "video desc...",
          id: 1221,
        type: "video",
       cover: "img/video_placeholder.jpg"
     }
 ];
 
+var options = {
+  shouldSort: true,
+  threshold: 0.7,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    "title",
+    "desc"
+  ]
+};
+
+var fuse = new Fuse(localMedia, options);
+
 $("#browser_projects_search").bind('input', function(){
 
     $("#browser_projects").html(" ");
 
-    localMedia.forEach(function (entry) {
-        if(entry.title) {
-            $("#browser_projects").append("<li class='project'><img class='thumbnail' src='" +
-             entry.cover + "'/><div class='info'><p class='title'>" + entry.title + "</p></div></li>");
+    query = $("#browser_projects_search").val();
+    var results = fuse.search(query);
 
-        }
+    results.forEach(function (entry) {
+        $("#browser_projects").append("<li class='project' data-path='"+ entry.path +"'>"+
+             "<img class='thumbnail' src='"+ entry.cover +"'><div class='info'><p class='title'>" + entry.title + "</p></div></li>");
     });
+    
 });
 
 $("#browser_projects").sortable();
 $("#preview-container").draggable();
-
-$("#preview-container").droppable({
-    drop: function (event, ui) {
-        ui.draggable.fadeOut("slow");
-        $(".controls").append("<li class='playlist-item'>" + ui.draggable.html() + "</li>");
-        ui.draggable.fadeIn("slow");
-    }
-});
+$("#preview-container").resizable({ aspectRatio: true });
 
 // establish video player
 VideoPlayer = $("#mediaPlayer").get(0);
 
+setTitle = function (title) {
+    $("#title").html(title);
+}
+
 shortcut.add("k", function () {
     VideoPlayer.paused ? VideoPlayer.play() : VideoPlayer.pause();
+});
+
+// drag and drop video sources
+$("#preview-container").droppable({
+    drop: function (event, ui) {
+        ui.draggable.hide();
+
+    	  var vidSrc = ui.draggable.attr("data-path");
+    	  setTitle(vidSrc);    	      	  
+         
+        var vidTitle = ui.draggable.find(".title").text();
+        setTitle(vidTitle);
+        alert(VideoPlayer);
+        //$("#fullTime").html(VideoPlayer);
+
+        VideoPlayer.ontimeupdate = function () {
+            $("#currentTime").html(VideoPlayer.currentTime);
+        };
+
+
+        VideoPlayer.src = vidSrc;
+        alert(videoPlayer.duration);
+
+        VideoPlayer.play();
+        
+        $(".controls").append("<li class='playlist-item dropped'>" + ui.draggable.html() + "</li>");
+        ui.draggable.fadeIn("slow");
+    }
 });
 
 // RequestAnimFrame: a browser API for getting smooth animations
@@ -84,227 +171,3 @@ window.requestAnimFrame = (function(){
 			window.setTimeout(callback, 1000 / 60);
 		  };
 })();
-
-var canvas = document.getElementById("mediaCanvas");
-
-// Initialize the context of the canvas
-var ctx = canvas.getContext("2d");
-
-// Set the canvas width and height to occupy full window
-var W = window.innerWidth, H = window.innerHeight;
-canvas.width = W;
-canvas.height = H;
-
-var video;
-var copy;
-var copycanvas;
-var draw;
-
-var TILE_WIDTH = 32;
-var TILE_HEIGHT = 24;
-var TILE_CENTER_WIDTH = 16;
-var TILE_CENTER_HEIGHT = 12;
-var SOURCERECT = {x:0, y:0, width:0, height:0};
-var PAINTRECT = {x:0, y:0, width:1000, height:600};
-
-function init(){
-   video = VideoPlayer;	
-	
-	copycanvas = document.getElementById('mediasourcecopy');
-	copy = copycanvas.getContext('2d');
-	var outputcanvas = canvas;
-	draw = outputcanvas.getContext('2d');
-	setInterval("processFrame()", 33);
-}
-function createTiles(){
-	var offsetX = TILE_CENTER_WIDTH+(PAINTRECT.width-SOURCERECT.width)/2;
-	var offsetY = TILE_CENTER_HEIGHT+(PAINTRECT.height-SOURCERECT.height)/2;
-	var y=0;
-	while(y < SOURCERECT.height){
-		var x=0;
-		while(x < SOURCERECT.width){
-			var tile = new Tile();
-			tile.videoX = x;
-			tile.videoY = y;
-			tile.originX = offsetX+x;
-			tile.originY = offsetY+y;
-			tile.currentX = tile.originX;
-			tile.currentY = tile.originY;
-			tiles.push(tile);
-			x+=TILE_WIDTH;
-		}
-		y+=TILE_HEIGHT;
-	}
-}
-
-var RAD = Math.PI/180;
-var randomJump = false;
-var tiles = [];
-var debug = false;
-function processFrame(){
-	if(!isNaN(video.duration)){
-		if(SOURCERECT.width == 0){
-			SOURCERECT = {x:0,y:0,width:video.videoWidth,height:video.videoHeight};
-			createTiles();
-		}
-		//this is to keep my sanity while developing
-		if(randomJump){
-			randomJump = false;
-			video.currentTime = Math.random()*video.duration;
-		}
-		//loop
-		if(video.currentTime == video.duration){
-			video.currentTime = 0;
-		}
-	}
-	var debugStr = "";
-	//copy tiles
-	copy.drawImage(video, 0, 0);
-	draw.clearRect(PAINTRECT.x, PAINTRECT.y,PAINTRECT.width,PAINTRECT.height);
-	
-	for(var i=0; i<tiles.length; i++){
-		var tile = tiles[i];
-		if(tile.force > 0.0001){
-			//expand
-			tile.moveX *= tile.force;
-			tile.moveY *= tile.force;
-			tile.moveRotation *= tile.force;
-			tile.currentX += tile.moveX;
-			tile.currentY += tile.moveY;
-			tile.rotation += tile.moveRotation;
-			tile.rotation %= 360;
-			tile.force *= 0.9;
-			if(tile.currentX <= 0 || tile.currentX >= PAINTRECT.width){
-				tile.moveX *= -1;
-			}
-			if(tile.currentY <= 0 || tile.currentY >= PAINTRECT.height){
-				tile.moveY *= -1;
-			}
-		}else if(tile.rotation != 0 || tile.currentX != tile.originX || tile.currentY != tile.originY){
-			//contract
-			var diffx = (tile.originX-tile.currentX)*0.2;
-			var diffy = (tile.originY-tile.currentY)*0.2;
-			var diffRot = (0-tile.rotation)*0.2;
-			
-			if(Math.abs(diffx) < 0.5){
-				tile.currentX = tile.originX;
-			}else{
-				tile.currentX += diffx;
-			}
-			if(Math.abs(diffy) < 0.5){
-				tile.currentY = tile.originY;
-			}else{
-				tile.currentY += diffy;
-			}
-			if(Math.abs(diffRot) < 0.5){
-				tile.rotation = 0;
-			}else{
-				tile.rotation += diffRot;
-			}
-		}else{
-			tile.force = 0;
-		}
-		draw.save();
-		draw.translate(tile.currentX, tile.currentY);
-		draw.rotate(tile.rotation*RAD);
-		draw.drawImage(copycanvas, tile.videoX, tile.videoY, TILE_WIDTH, TILE_HEIGHT, -TILE_CENTER_WIDTH, -TILE_CENTER_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
-		draw.restore();
-	}
-	if(debug){
-		debug = false;
-		document.getElementById('trace').innerHTML = debugStr;
-	}
-}
-
-function explode(x, y){
-	for(var i=0; i<tiles.length; i++){
-		var tile = tiles[i];
-		
-		var xdiff = tile.currentX-x;
-		var ydiff = tile.currentY-y;
-		var dist = Math.sqrt(xdiff*xdiff + ydiff*ydiff);
-		
-		var randRange = 220+(Math.random()*30);
-		var range = randRange-dist;
-		var force = 3*(range/randRange);
-		if(force > tile.force){
-			tile.force = force;
-			var radians = Math.atan2(ydiff, xdiff);
-			tile.moveX = Math.cos(radians);
-			tile.moveY = Math.sin(radians);
-			tile.moveRotation = 0.5-Math.random();
-		}
-	}
-	tiles.sort(zindexSort);
-	processFrame();
-}
-function zindexSort(a, b){
-	return (a.force-b.force);
-}
-
-function dropBomb(evt, obj){
-	var posx = 0;
-	var posy = 0;
-	var e = evt || window.event;
-	if (e.pageX || e.pageY){
-		posx = e.pageX;
-		posy = e.pageY;
-	}else if (e.clientX || e.clientY) {
-		posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-		posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-	}
-	var canvasX = posx-obj.offsetLeft;
-	var canvasY = posy-obj.offsetTop;
-	explode(canvasX, canvasY);
-}
-
-function Tile(){
-	this.originX = 0;
-	this.originY = 0;
-	this.currentX = 0;
-	this.currentY = 0;
-	this.rotation = 0;
-	this.force = 0;
-	this.z = 0;
-	this.moveX= 0;
-	this.moveY= 0;
-	this.moveRotation = 0;
-	
-	this.videoX = 0;
-	this.videoY = 0;
-}
-
-
-/*
-	getPixel
-	return pixel object {r,g,b,a}
-*/
-function getPixel(imageData, x, y){
-	var data = imageData.data;
-	var pos = (x + y * imageData.width) * 4;
-	return {r:data[pos], g:data[pos+1], b:data[pos+2], a:data[pos+3]}
-}
-/*
-	setPixel
-	set pixel object {r,g,b,a}
-*/
-function setPixel(imageData, x, y, pixel){
-	var data = imageData.data;
-	var pos = (x + y * imageData.width) * 4;
-	data[pos] = pixel.r;
-	data[pos+1] = pixel.g;
-	data[pos+2] = pixel.b;
-	data[pos+3] = pixel.a;
-}
-/*
-	copyPixel
-	faster then using getPixel/setPixel combo
-*/
-function copyPixel(sImageData, sx, sy, dImageData, dx, dy){
-	var spos = (sx + sy * sImageData.width) * 4;
-	var dpos = (dx + dy * dImageData.width) * 4;
-	dImageData.data[dpos] = sImageData.data[spos];     //R
-	dImageData.data[dpos+1] = sImageData.data[spos+1]; //G
-	dImageData.data[dpos+2] = sImageData.data[spos+2]; //B
-	dImageData.data[dpos+3] = sImageData.data[spos+3]; //A
-}
